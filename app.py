d4b22b0066fbf760e80d51262baf5a3fea554432
IyswaryaGopalakrishnan <iyswarya1996@gmail.com>
2025-03-23 14:29:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -38,6 +39,10 @@ def get_weather(city):
     else:
         return "unavailable at the moment."
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))  # Get the port assigned by Render
+    app.run(debug=True, host="0.0.0.0", port=port)
+
 
